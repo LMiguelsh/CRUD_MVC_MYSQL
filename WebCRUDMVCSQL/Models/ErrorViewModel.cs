eee20338613cdec2d79369cbfdd941c318763b3a
Luis <luis.miguel1500@gmail.com>
2022-08-30 16:26:11 -0300

    CRUD MVC SQL

---
+++ WebCRUDMVCSQL/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace WebCRUDMVCSQL.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
